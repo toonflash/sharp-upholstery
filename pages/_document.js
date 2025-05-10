91b49ad064de5e1e6c55f2d68e4d4ba11ec6b913
Jason Shevolup <38705362+toonflash@users.noreply.github.com>
2025-05-10 11:09:10 -0700

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body styles="letter-spacing: 1px;">
         <Main />
         <NextScript />
       </body>
